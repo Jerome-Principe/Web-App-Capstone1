d9137d4783cabd7763a68050f1457a30157feba3
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-09 04:36:10 +0800

    Update RequestMembershipController.php

--- app/Http/Controllers/RequestMembershipController.php
+++ app/Http/Controllers/RequestMembershipController.php
@@ -15,6 +15,19 @@ class RequestMembershipController extends Controller
     }
     public function store(Request $request)
     {
+        $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email|unique:request_memberships',
+            'date' => 'required|date',
+            'gender' => 'required|string',
+            'age' => 'nullable|integer',
+            'weight' => 'nullable|numeric',
+            'height' => 'nullable|numeric',
+            'mobile' => 'required|string',
+            'membership_type' => 'required|string',
+        ]);
+
         $membership = RequestMembership::create(
             $request->all()
         );
