d8528ba35eff0c2cba678fef6accc10c5ddc000a
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-11 04:23:59 +0800

    update membership-payment-list links

--- resources/views/membership-payment-list.blade.php
+++ resources/views/membership-payment-list.blade.php
@@ -142,8 +142,8 @@
                             <td class="text-center">{{ $payment->account_name }}</td>
                             <td class="text-center">{{ $payment->reference_number }}</td>
                             <td class="text-center">
-                                <a href="{{ $payment->proof_of_payment_url }}" target="_blank">
-                                    View Proof
+                                <a href="{{ asset('storage/' . basename($payment->proof_of_payment_url)) }}"
+                                    target="_blank">View Proof
                                 </a>
                             </td>
                         </tr>
