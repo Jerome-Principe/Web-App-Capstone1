1d4a80a25f56fc812f6c0602514376f8ee1eb418
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-18 01:30:55 +0800

    Update PendingAppointmentController.php

--- app/Http/Controllers/PendingAppointmentController.php
+++ app/Http/Controllers/PendingAppointmentController.php
@@ -67,7 +67,7 @@ class PendingAppointmentController extends Controller
     public function appointmentList()
     {
         // Fetch both confirmed and declined appointments
-        $appointments = PendingAppointment::whereIn('status', ['Confirmed', 'Cancelled'])
+        $appointments = PendingAppointment::whereIn('status', ['Approved', 'Declined'])
             ->with(['instructor', 'pendingMembership'])
             ->get();
 
