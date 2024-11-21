520a4787f3256f8a3fdd0187593a7a8d4e5bb788
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-21 18:55:38 +0800

    Update appointment-list.blade.php

--- resources/views/appointment-list.blade.php
+++ resources/views/appointment-list.blade.php
@@ -135,7 +135,6 @@
                     <tr>
                         <th class="text-center"><input type="checkbox" onclick="toggleSelectAll(this)" /></th>
                         <th class="text-center">ID</th>
-                        <th class="text-center">User</th>
                         <th class="text-center">Instructor</th>
                         <th class="text-center">Date</th>
                         <th class="text-center">Time</th>
@@ -154,7 +153,6 @@
                             <td class="text-center">
                                 {{ ($appointments->currentPage() - 1) * $appointments->perPage() + $loop->index + 1 }}
                             </td>
-                            <td class="text-center">{{ $appointment->id }}</td>
                             <td class="text-center">{{ $appointment->pendingMembership->name }}</td>
                             <td class="text-center">
                                 {{ $appointment->instructor->first_name . ' ' . $appointment->instructor->last_name }}
