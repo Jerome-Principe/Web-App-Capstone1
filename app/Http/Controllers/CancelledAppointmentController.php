bab1fa827620c2dcbfe84a0378b0dfe5d047d8c1
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-22 00:22:46 +0800

    update controller and blade

--- app/Http/Controllers/CancelledAppointmentController.php
+++ app/Http/Controllers/CancelledAppointmentController.php
@@ -20,41 +20,38 @@ class CancelledAppointmentController extends Controller
         $request->validate([
             'user_id' => 'required|integer',
             'instructor_name' => 'required|string|max:255',
-            'selected_date' => 'required|date_format:m/d/Y',
-            'selected_time' => 'required|string|max:255',
+            'selected_date' => 'required|date_format:Y-m-d',
+            'selected_time' => 'required|string',
             'payment_method' => 'required|string|max:255',
             'proof_of_payment' => 'required|string',
             'reason' => 'required|string|max:255',
         ]);
 
         try {
-            $formattedDate = Carbon::createFromFormat('m/d/Y', $request->selected_date)->format('Y-m-d');
-            $formattedTime = Carbon::createFromFormat('g:i:s A', $request->selected_time)->format('H:i:s');
+            // Format date and time
+            $formattedDate = Carbon::createFromFormat('Y-m-d', $request->selected_date)->format('Y-m-d');
+            $formattedTime = Carbon::createFromFormat('H:i:s', $request->selected_time)->format('H:i:s');
 
-            // Handle the file upload
-            if ($request->hasFile('proof_of_payment')) {
-                $proofOfPayment = $request->file('proof_of_payment');
-            }
-
+            // Store appointment
             $cancelledAppointment = new CancelledAppointment();
             $cancelledAppointment->user_id = $request->user_id;
             $cancelledAppointment->instructor_name = $request->instructor_name;
             $cancelledAppointment->selected_date = $formattedDate;
             $cancelledAppointment->selected_time = $formattedTime;
             $cancelledAppointment->payment_method = $request->payment_method;
+            $cancelledAppointment->proof_of_payment = $request->proof_of_payment; // Save the proof of payment path
             $cancelledAppointment->reason = $request->reason;
-            $cancelledAppointment->proof_of_payment = $proofOfPayment;
             $cancelledAppointment->save();
 
             return response()->json([
                 'status' => 'success',
-                'message' => 'Cancellation submitted successfully.'
+                'message' => 'Cancellation submitted successfully.',
             ], 200);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
                 'message' => 'Error submitting cancellation.',
-                'exception' => $e->getMessage()
+                'exception' => $e->getMessage(),
             ], 500);
         }
     }
