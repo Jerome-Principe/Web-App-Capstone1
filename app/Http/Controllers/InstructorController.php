a5e6591c7a1ef978bed8236c117158393637ef67
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-22 22:15:10 +0800

    add a modal in instructor list blade and controller

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -9,7 +9,7 @@ class InstructorController extends Controller
 {
     public function index(Request $request)
     {
-        $instructors = Instructor::all();
+        $instructors = Instructor::paginate(10); // Adjust the number of items per page as needed
 
         // Check if the request expects JSON
         if ($request->wantsJson()) {
