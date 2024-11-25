2db1fce92914fe8105a676e42ae58de124087981
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-11-26 02:04:56 +0800

    Update MembershipPendingController.php

--- app/Http/Controllers/MembershipPendingController.php
+++ app/Http/Controllers/MembershipPendingController.php
@@ -50,50 +50,9 @@ class MembershipPendingController extends Controller
         return view('membership-list', compact('memberships'));
     }
 
-
     public function mGetMembershipPending()
     {
         return PendingMembership::where('status', 'Pending')->get();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
     }
 
     /**
