307d2c6ad271676159894f10f4e8fd3cb07d3e61
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-12-05 17:10:06 +0800

    Update admin-sidebar.blade.php

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -71,10 +71,10 @@
                         <a href="instructors">View Instructor</a>
                     </li>
                     <li>
-                        <a href="/appointments">View Appointment List</a>
+                        <a href="/appointments/appointments">View Appointment List</a>
                     </li>
                     <li>
-                        <a href="/appointment-pending-list">Pending Appointment</a>
+                        <a href="/appointments/appointment-pending-list">Pending Appointment</a>
                     </li>
                     <li>
                         <a href="/cancelled">Cancelled Appointment</a>
