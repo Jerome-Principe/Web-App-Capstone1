7e2a79971715f9098d30c48fe3b96d463d4d49f5
jeromeprincipe <jeromeprincipe08@gmail.com>
2024-12-11 19:44:56 +0800

    Update PendingMembership.php

--- app/Models/PendingMembership.php
+++ app/Models/PendingMembership.php
@@ -50,16 +50,4 @@ class PendingMembership extends Model
     {
         return "{$this->first_name} {$this->last_name}";
     }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::deleting(function ($pendingMembership) {
-            // Delete related records when the membership is soft-deleted
-            $pendingMembership->requestMembership()->forceDelete();
-            $pendingMembership->medicalForm()->forceDelete();
-            $pendingMembership->membershipPayments()->forceDelete();
-        });
-    }
 }
