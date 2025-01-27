e5902422e7047f975d91b0ac672d7dd0aa884287
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-01-27 18:28:35 +0800

    Update WalkinController.php

--- app/Http/Controllers/WalkinController.php
+++ app/Http/Controllers/WalkinController.php
@@ -11,7 +11,7 @@ class WalkinController extends Controller
     // Display a paginated list of walk-in clients with the total amount
     public function index()
     {
-        $walkins = Walkin::paginate(9); // Paginate walk-ins (9 per page)
+        $walkins = Walkin::paginate(5); // Paginate walk-ins (9 per page)
         $totalAmount = Walkin::sum('amount'); // Calculate total amount of walk-ins
 
         return view('walkin-client-list', compact('walkins', 'totalAmount'));
