77a22b765b1b6dac513795256789a71e0b9265cf
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-02-18 14:19:15 +0800

    Update fitdroid.blade.php

--- resources/views/fitdroid.blade.php
+++ resources/views/fitdroid.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
+    <link rel="icon" href="{{asset('assets/images/muscle.png')}}" type="image/png">
 
     <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
 
