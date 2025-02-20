be91305c0d02dd3977ecb033dbb5843ba7701edc
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-02-20 21:57:14 +0800

    update related to drinks

--- resources/views/inventory-drinks-create.blade.php
+++ resources/views/inventory-drinks-create.blade.php
@@ -172,6 +172,10 @@
                     <input type="date" id="date" name="date" required>
                 </div>
 
+                <div class="input-field">
+                    <label for="time">Time:</label>
+                    <input type="time" id="time" name="time" required>
+                </div>
             </div>
 
             @if(session('success'))
