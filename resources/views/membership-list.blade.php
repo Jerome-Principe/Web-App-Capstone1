6ec599eff324050687da094f487dc98ffd54843d
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-03-06 13:17:45 +0800

    Update membership-list.blade.php

--- resources/views/membership-list.blade.php
+++ resources/views/membership-list.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Membership List</title>
     <style>
@@ -182,7 +181,9 @@
                                 <td class="text-center">
                                     {{ $membership->start_date ? \Carbon\Carbon::parse($membership->start_date)->format('Y-m-d') : 'N/A' }}
                                 </td>
-                                <td class="text-center">{{ $membership->expiry_date }}</td> <!-- Expiry Date -->
+                                <td class="text-center">
+                                    {{ $membership->expiry_date ? \Carbon\Carbon::parse($membership->expiry_date)->format('Y-m-d') : 'N/A' }}
+                                </td>
                                 <td class="text-center">
                                     {{ ucfirst(optional($membership->requestMembership)->membership_type ?? 'N/A') }}
                                 </td>
