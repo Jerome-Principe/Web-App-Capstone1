77d17483075c1e97cb0156cf8395f700b2968fbd
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-03-12 17:45:50 +0800

    update master.blade and dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -277,6 +277,18 @@
                 </div>
                 <!-- Content Wrapper END -->
 
+                <!-- Footer START -->
+                <footer class="content-footer">
+                    <div class="footer">
+                        <div class="copyright">
+                            <span>Copyright © 2018 <b class="text-dark">UIdeck</b>. All Right Reserved</span>
+                            <span class="go-right">
+                                <a href="" class="text-gray">Term &amp; Conditions</a>
+                                <a href="" class="text-gray">Privacy &amp; Policy</a>
+                            </span>
+                        </div>
+                    </div>
+                </footer>
             </div>
             <!-- Page Container END -->
         </div>
