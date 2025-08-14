f22c334456c006b3949142514d12086de98a2cb5
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-08-14 22:07:30 +0800

    Update expenses.blade.php

--- resources/views/expenses.blade.php
+++ resources/views/expenses.blade.php
@@ -507,7 +507,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="editExpenseModalLabel">Edit Expense</h5>
+                        <h5 class="modal-title" id="editExpenseModalLabel">Edit Expenses</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <form method="POST" id="editExpenseForm">
