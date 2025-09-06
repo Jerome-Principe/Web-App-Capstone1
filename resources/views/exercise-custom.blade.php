73942b2ee3f599e546ad1a9d4734112828b16d19
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-09-06 18:28:25 +0800

    add muscle icon for resources and inventory

--- resources/views/exercise-custom.blade.php
+++ resources/views/exercise-custom.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="{{asset('assets/images/muscle.png')}}" type="image/png">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Exercise Custom List</title>
     <style>
@@ -641,7 +642,7 @@
                 @foreach($exercisesCustom as $exerciseCustom)
                     updateExercises('{{ $exerciseCustom->id }}', '{{ $exerciseCustom->exercise }}');
                 @endforeach
-                                            });
+                                                });
         </script>
 
 
--- resources/views/exercise.blade.php
+++ resources/views/exercise.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="icon" href="{{asset('assets/images/muscle.png')}}" type="image/png">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <title>Exercise Default List</title>
     <style>
@@ -592,7 +593,7 @@
                 @foreach($exercises as $exercise)
                     updateExercises('{{ $exercise->id }}', '{{ $exercise->exercise }}');
                 @endforeach 
-                                                });
+                                                        });
 
             function updateExercises(exerciseId = '', selectedExercise = '') {
                 const type = document.getElementById('type' + exerciseId).value;
