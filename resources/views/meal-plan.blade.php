58466c35b222ed9f02c084dcdd0421955726276b
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-09-06 18:18:47 +0800

    add muscle.png for instructor-list.blade.php, walkin-client-list.blade.php and edit title for meal-plan

--- resources/views/meal-plan.blade.php
+++ resources/views/meal-plan.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Meal Plan</title>
+    <title>Meal Plan Default List</title>
     <style>
         /* Minimalist Global Styles */
         body {
