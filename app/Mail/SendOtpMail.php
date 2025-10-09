278348d2b704ca14a9b06423e1fcc4c16888c74d
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-10-09 22:33:04 +0800

    update otp mail

--- app/Mail/SendOtpMail.php
+++ app/Mail/SendOtpMail.php
@@ -30,7 +30,7 @@ class SendOtpMail extends Mailable
     {
         return new Envelope(
             subject: 'Your OTP Code',
-            from: new Address('legit@limitlessfitnesstudio.com', 'Limitless Fitness Studio'),
+            from: new Address('gym@limitlessfitnesstudio.com', 'Limitless Fitness Studio'),
         );
     }
 
