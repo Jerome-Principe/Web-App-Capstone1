b73c291cf9a60f26946fe1705cd63845650a7399
jeromeprincipe <jeromeprincipe08@gmail.com>
2025-10-16 16:43:50 +0800

    Update Upload process Only in add new instructor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -75,7 +75,7 @@ class InstructorController extends Controller
                 return redirect()->back()->with('error', 'Invalid image file. Please try again.');
             }
 
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $imageName = 'instructor_profile/' . time() . '.' . $image->getClientOriginalExtension();
 
             // Log upload attempt
             \Log::info('Attempting to upload image: ' . $imageName);
@@ -83,8 +83,8 @@ class InstructorController extends Controller
             \Log::info('Image mime type: ' . $image->getMimeType());
 
             try {
-                // Store the image in storage/app/public/instructor_profile folder
-                $storedPath = $image->storeAs('public/instructor_profile', $imageName);
+                // Store the image in storage/app/public/public/instructor_profile folder
+                $storedPath = $image->storeAs('public/public/', $imageName);
 
                 if ($storedPath) {
                     // Remove 'public/' from the path for database storage
@@ -157,10 +157,10 @@ class InstructorController extends Controller
             }
 
             $image = $request->file('profile_image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
+            $imageName = 'instructor_profile/' . time() . '.' . $image->getClientOriginalExtension();
 
-            // Store the image in storage/app/public/instructor_profile folder
-            $storedPath = $image->storeAs('public/instructor_profile', $imageName);
+            // Store the image in storage/app/public/public/instructor_profile folder
+            $storedPath = $image->storeAs('public/public/', $imageName);
 
             if ($storedPath) {
                 // Remove 'public/' from the path for database storage
