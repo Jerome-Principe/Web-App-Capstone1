c917e5d758fbb29d1394e6a9a039a63e1c695350
jeromeprincipe-git <jeromeprincipe.dev@gmail.com>
2026-08-27 23:48:23 +0800

    Update fitdroid.blade.php
    Update title to FitDroid

--- resources/views/fitdroid.blade.php
+++ resources/views/fitdroid.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
 
-    <title>Fitdroid</title>
+    <title>FitDroid</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
